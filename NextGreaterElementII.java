0c5ff225419ecb2b3541bc9f980b8a21a2913854
chovie tinkerer <Egor821@yandex.ru>
2026-02-21 15:01:56 +0300

    503. Next Greater Element II

---
+++ NextGreaterElementII.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class NextGreaterElementII {
+    public int[] nextGreaterElements(int[] nums) {
+        int[] result = new int[nums.length];
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            stack.push(nums[i]);
+        }
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums[i]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums[i]);
+        }
+
+        return result;
+    }
+}
