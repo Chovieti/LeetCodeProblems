dd5f982687900440ae10e73ea4374c0bf2825fdf
chovie tinkerer <Egor821@yandex.ru>
2026-04-14 01:53:03 +0300

    1848. Minimum Distance to the Target Element

---
+++ MinimumDistanceToTheTargetElement.java
@@ -0,0 +1,9 @@
+public class MinimumDistanceToTheTargetElement {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) result = Math.min(result, Math.abs(i - start));
+        }
+        return result;
+    }
+}
