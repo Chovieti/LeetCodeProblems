df699ef978b06eda4c5c67acca8fd337302609c1
chovie tinkerer <Egor821@yandex.ru>
2026-04-16 18:23:59 +0300

    3741. Minimum Distance Between Three Equal Elements II

---
+++ MinimumDistanceBetweenThreeEqualElementsII.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MinimumDistanceBetweenThreeEqualElementsII {
+    public int minimumDistance(int[] nums) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.get(nums[i]).add(i);
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                map.put(nums[i], list);
+            }
+        }
+        int result = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, List<Integer>> en : map.entrySet()) {
+            if (en.getValue().size() < 3) continue;
+            List<Integer> list = en.getValue();
+            for (int i = 0; i < list.size() - 2; i++) {
+                int x = list.get(i);
+//               for (int j = i + 1; j < list.size() - 1; j++) {
+//                   int y = list.get(j);
+//                   for (int k = j + 1; k < list.size(); k++) {
+//                       int z = list.get(k);
+//                       int tmpRes = Math.abs(x - y) + Math.abs(y - z) + Math.abs(z - x);
+//                       result = Math.min(result, tmpRes);
+//                   }
+//               }
+                int y = list.get(i + 1);
+                int z = list.get(i + 2);
+                int tmpRes = Math.abs(x - y) + Math.abs(y - z) + Math.abs(z - x);
+                result = Math.min(result, tmpRes);
+            }
+        }
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+}
