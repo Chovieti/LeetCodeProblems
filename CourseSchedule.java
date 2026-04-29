b573d6f171bcb3bd2764ead71cd94d5f5e0e6ac1
chovie tinkerer <Egor821@yandex.ru>
2026-04-29 22:21:59 +0300

    207. Course Schedule

--- CourseSchedule.java
+++ CourseSchedule.java
@@ -1,52 +1,82 @@
-import java.util.Stack;
+import java.util.*;
 
 public class CourseSchedule {
+    // Более чистый вариант
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[][] adjancency = new int[numCourses][];  // i - для чего, j - требования
-        for (int i = 0; i < numCourses; i++) adjancency[i] = new int[numCourses];
+        Map<Integer, List<Integer>> graph = new HashMap<>(numCourses);
         for (int i = 0; i < prerequisites.length; i++) {
-            int y = prerequisites[i][0];
-            int x = prerequisites[i][1];
-            if (y == x) return false;
-            adjancency[y][x] = 1;
-        }
-        int[] status = new int[numCourses];  // 0 - не посещена, 1 - в процессе, 2 - посещена
-        Stack<Integer> dfs = new Stack<>();
-        for (int i = 0; i < numCourses; i++) {
-            status[i] = 1;
-            for (int j = 0; j < numCourses; j++) {
-                if (adjancency[i][j] == 1 && status[j] == 0) {
-                    dfs.push(j);
-                    status[j] = 1;
-                    if (!DFSReccursive(numCourses, dfs, adjancency, status)) return false;
-                }
+            if (graph.containsKey(prerequisites[i][0])) {
+                graph.get(prerequisites[i][0]).add(prerequisites[i][1]);
+            } else {
+                graph.put(prerequisites[i][0], new ArrayList<>(List.of(prerequisites[i][1])));
             }
-            status[i] = 2;
+        }
+        int[] state = new int[numCourses];
+        for (int i = 0; i < state.length; i++) {
+            if (!dfs(graph, state, i)) return false;
         }
         return true;
     }
 
-    private boolean DFSReccursive(int numCourses, Stack<Integer> dfs, int[][] adjancency, int[] status) {
-        while (!dfs.isEmpty()) {
-            int cur = dfs.peek();
-            for (int k = 0; k < numCourses; k++) {
-                if (cur == k) continue;
-                if (adjancency[cur][k] == 1) {
-                    switch (status[k]) {
-                        case 0 -> {
-                            dfs.push(k);
-                            status[k] = 1;
-                            if (!DFSReccursive(numCourses, dfs, adjancency, status)) return false;
-                        }
-                        case 1 -> {
-                            return false;
-                        }
-                    }
-                }
+    private boolean dfs(Map<Integer, List<Integer>> graph, int[] state, int node) {
+        if (state[node] == 1) return false;
+        if (state[node] == 2) return true;
+        state[node] = 1;
+        if (graph.containsKey(node)) {
+            for (int neighbor : graph.get(node)) {
+                if (!dfs(graph, state, neighbor)) return false;
             }
-            status[cur] = 2;
-            if (!dfs.isEmpty()) dfs.pop();
         }
+        state[node] = 2;
         return true;
     }
+//    Старый вариант
+//    public boolean canFinish(int numCourses, int[][] prerequisites) {
+//        int[][] adjancency = new int[numCourses][];  // i - для чего, j - требования
+//        for (int i = 0; i < numCourses; i++) adjancency[i] = new int[numCourses];
+//        for (int i = 0; i < prerequisites.length; i++) {
+//            int y = prerequisites[i][0];
+//            int x = prerequisites[i][1];
+//            if (y == x) return false;
+//            adjancency[y][x] = 1;
+//        }
+//        int[] status = new int[numCourses];  // 0 - не посещена, 1 - в процессе, 2 - посещена
+//        Stack<Integer> dfs = new Stack<>();
+//        for (int i = 0; i < numCourses; i++) {
+//            status[i] = 1;
+//            for (int j = 0; j < numCourses; j++) {
+//                if (adjancency[i][j] == 1 && status[j] == 0) {
+//                    dfs.push(j);
+//                    status[j] = 1;
+//                    if (!DFSReccursive(numCourses, dfs, adjancency, status)) return false;
+//                }
+//            }
+//            status[i] = 2;
+//        }
+//        return true;
+//    }
+//
+//    private boolean DFSReccursive(int numCourses, Stack<Integer> dfs, int[][] adjancency, int[] status) {
+//        while (!dfs.isEmpty()) {
+//            int cur = dfs.peek();
+//            for (int k = 0; k < numCourses; k++) {
+//                if (cur == k) continue;
+//                if (adjancency[cur][k] == 1) {
+//                    switch (status[k]) {
+//                        case 0 -> {
+//                            dfs.push(k);
+//                            status[k] = 1;
+//                            if (!DFSReccursive(numCourses, dfs, adjancency, status)) return false;
+//                        }
+//                        case 1 -> {
+//                            return false;
+//                        }
+//                    }
+//                }
+//            }
+//            status[cur] = 2;
+//            if (!dfs.isEmpty()) dfs.pop();
+//        }
+//        return true;
+//    }
 }
