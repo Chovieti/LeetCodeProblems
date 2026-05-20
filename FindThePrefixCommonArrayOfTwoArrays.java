740e2670408fa26b038abe4f7ba0782d708a003b
Chovieti <Egor821@yandex.ru>
2026-05-20 04:25:35 +0300

    2657. Find the Prefix Common Array of Two Arrays

--- FindThePrefixCommonArrayOfTwoArrays.java
+++ FindThePrefixCommonArrayOfTwoArrays.java
@@ -1,17 +1,30 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class FindThePrefixCommonArrayOfTwoArrays {
+    // Битовая маска для просмотра частоты, работает за счет ограничений в n <= 50
+//    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+//        int n = A.length, count = 0;
+//        long freq = 0L;
+//        int[] C = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            if (((freq >> A[i]) & 1) == 1) count++;
+//            freq |= 1L << A[i];
+//            if (((freq >> B[i]) & 1) == 1) count++;
+//            freq |= 1L << B[i];
+//            C[i] = count;
+//        }
+//        return C;
+//    }
+
+    // Более скупой по памяти вариант, где я даже не создаю новый массив, а переписываю старый
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        int n = A.length;
-        Map<Integer, Integer> map = new HashMap<>(n);
-        int count = 0;
-        int[] C = new int[n];
+        int n = A.length, count = 0;
+        long freq = 0L;
         for (int i = 0; i < n; i++) {
-            if (map.merge(A[i], 1, Integer::sum) >= 2) count++;
-            if (map.merge(B[i], 1, Integer::sum) >= 2) count++;
-            C[i] = count;
+            if (((freq >> A[i]) & 1) == 1) count++;
+            freq |= 1L << A[i];
+            if (((freq >> B[i]) & 1) == 1) count++;
+            freq |= 1L << B[i];
+            A[i] = count;
         }
-        return C;
+        return A;
     }
 }
